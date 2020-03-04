56a5dd441721ce5d6ebaa5f3cde53870e84fab2c
Jerwin <jerwinfernandez04@gmail.com>
2020-03-05 04:39:56 +0800

    Completed Core class

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,5 @@
-<!-- require all the libraries / config / helper -->
+<?php
+  // load all the libraries / config / helper 
+  require_once 'libraries/Core.php';
+  require_once 'libraries/Controller.php';
+  require_once 'libraries/Database.php';
