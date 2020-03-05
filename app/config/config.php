b6794fdb961805b517ab84a28195adedd47147c1
Jerwin <jerwinfernandez04@gmail.com>
2020-03-05 08:05:16 +0800

    Completed database class

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // App root where the app folder is
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL root
+  define('URLROOT', 'http://localhost/php-mvc');
+  // site name
+  define('SITENAME', 'PHPMVC - JISF');
+
+  // DB params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', 'jrwnclyn');
+  define('DB_NAME', 'php-mvc');
